a3dfdb8b4cb618b8c552a6910bcb45f114e0d2db
robclm <robertocolomina@gmail.com>
2018-04-14 17:13:17 +0200

    models-bd shop v.1.0

--- MusicShop/admin.py
+++ MusicShop/admin.py
@@ -1,1 +1,10 @@
 # Register your models here.
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Artist)
+admin.site.register(Label)
+admin.site.register(Release)
+admin.site.register(ReleaseGroup)
+admin.site.register(Recording)
+admin.site.register(Work)
